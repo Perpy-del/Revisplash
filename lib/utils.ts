74053e2a0651e87fc4ccd049e3e237f6c25c5e56
Perpy-del <perpetualmeninwa@gmail.com>
2024-08-31 01:02:34 +0100

    fix: :bug: protect sensitive data by removing file from commit

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export const UNSPLASH_ACCESS_KEY: string = 'mjnrxC099Gpi_gptvO9fW_VoNPTW4UN3Hii-FC9WXEQ'
